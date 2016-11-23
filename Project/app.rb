551aa319b8256bee4a23eae6a8c8fbe13314c3fe
rakeshdrk <rakeshdrk29@gmail.com>
2016-11-23 00:44:04 -0500

    UI alerts/messaging

--- Project/app.rb
+++ Project/app.rb
@@ -1,6 +1,7 @@
 # myapp.rb
 require 'sinatra'
 require 'json'
+require 'twilio-ruby'
 get '/' do
 	erb :index
 end
@@ -37,4 +38,47 @@ get '/alerts' do
    @logFile = File.read("/tmp/alerts.log")
 	erb :alerts
    #@logFile.close
+end
+
+post '/setparams' do
+   @alert = params[:alert]
+   @perc = params[:perc]
+   if @alert == "mem" 
+    cmd = "/tmp/runmem.sh" +" "+@perc
+    system(cmd)
+   else
+    cmd = "/tmp/cpumem.sh" +" "+@perc
+    system(cmd)     
+   end
+   @account_sid = ''
+   @auth_token = ''
+
+   # set up a client to talk to the Twilio REST API 
+   @client = Twilio::REST::Client.new @account_sid, @auth_token
+
+   @client.account.messages.create({
+    :messaging_service_sid => '',
+    :to => '+188888888',
+    :body => 'Alert thresshold changed' +" "+@alert+" "+@perc 
+   })
+   redirect '/alerts'
+end
+
+post '/sendalerts' do
+ 
+   before do
+     request.body.rewind
+     @requeste = JSON.parse request.body.read
+   end
+   @account_sid = '' 
+   @auth_token = '' 
+ 
+   # set up a client to talk to the Twilio REST API 
+   @client = Twilio::REST::Client.new @account_sid, @auth_token 
+ 
+   @client.account.messages.create({
+    :messaging_service_sid => '', 
+    :to => '+18888888', 
+    :body => 'Test Message'
+   })
 end
